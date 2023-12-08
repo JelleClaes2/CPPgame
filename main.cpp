9b3cce0e6b15ade3ece1de45e7df1dba2257fbfe
Jelle Claes <jelleclaes0@gmail.com>
2023-12-08 15:19:40 +0100

    Usier input aantal Spelers

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include "spel.h"
+#include"iostream"
 
 int main(int argc, char *argv[])
 {
-    int aantalSpelers = 8;
+    int aantalSpelers;
+    std::cout << "Selecteer een aantal spelers tussen de 8 en de 18" << std::endl;
+    std::cin >> aantalSpelers;
+
+    while(1){
+        if(aantalSpelers>=8){
+            if(aantalSpelers <=18){
+                std::cout << "Het spel wordt aangemaakt" << std::endl;
+                break;
+            }
+            else {
+                std::cout << "Aantal spelers moet tussen de 8 en de 18 liggen" <<std::endl;
+                std::cin >> aantalSpelers;
+            }
+        } else {
+            std::cout << "Aantal spelers moet tussen de 8 en de 18 liggen" <<std::endl;
+            std::cin >> aantalSpelers;
+        }
+    }
 
     Spel* nieuwSpel = new Spel(aantalSpelers);
     nieuwSpel->VoegSpelersToe(aantalSpelers);
     nieuwSpel->nacht();
+
     return 0;
 }
